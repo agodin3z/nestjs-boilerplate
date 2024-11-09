1f54cea674dd08d4ce402e33697cd2ee3af90684
agodin3z <pistilloagodinez@gmail.com>
2024-11-08 21:45:54 -0600

    chore(interface): update app.controller.spec.ts

--- src/interface/controllers/app.controller.spec.ts
+++ src/interface/controllers/app.controller.spec.ts
@@ -1,23 +1,36 @@
+import { CqrsModule, QueryBus } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { HelloHandler } from '@application/queries/handlers/hello.handler';
 import { AppService } from '@domain/services/app.service';
 import { AppController } from './app.controller';
 
 describe('AppController', () => {
   let appController: AppController;
+  let queryBus: QueryBus;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [CqrsModule],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [HelloHandler, AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    queryBus = app.get<QueryBus>(QueryBus);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      const mockResponse = {
+        status: jest.fn(),
+      };
+      jest.spyOn(queryBus, 'execute').mockResolvedValue({
+        status: 200,
+        result: { message: 'Hello World!' },
+      });
+      const result = await appController.getHello(mockResponse as any);
+      expect(result.message).toBe('Hello World!');
     });
   });
 });
